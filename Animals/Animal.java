9dc7595ae6ee047c61a36d6c0e2c92b54614b27a
julia-kudri <frizzy18@gmail.com>
2024-11-22 14:21:11 +0300

    1.0.1

---
+++ Animals/Animal.java
@@ -0,0 +1,12 @@
+public abstract class Animal {
+    double weight;
+    double maxSatiety;     //(Макс насыщение)
+    double actualSatiety;     //(Фактическая сытость)
+
+
+    public Animal(){}
+
+
+    public void eat(){}
+    public void move(){}
+}
